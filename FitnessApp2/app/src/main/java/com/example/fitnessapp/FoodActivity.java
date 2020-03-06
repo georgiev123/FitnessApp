d6890b39f80058e7a92b88f002e3bc8786a73db6
georgiev123 <36740801+georgiev123@users.noreply.github.com>
2020-03-06 13:06:25 +0200

    ImportDB

--- FitnessApp2/app/src/main/java/com/example/fitnessapp/FoodActivity.java
+++ FitnessApp2/app/src/main/java/com/example/fitnessapp/FoodActivity.java
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.JsonObject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,9 +23,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class FoodActivity extends AppCompatActivity {
 
@@ -34,6 +47,7 @@ public class FoodActivity extends AppCompatActivity {
     private String TAG = "FoodActivity";
 
     private Button btnBarcodeScan;
+    private TextView tvTest;
 
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<Double> mGrams = new ArrayList<>();
@@ -50,6 +64,36 @@ public class FoodActivity extends AppCompatActivity {
         setContentView(R.layout.activity_food);
 
         mauth = FirebaseAuth.getInstance();
+
+        tvTest = findViewById(R.id.tvTEST);
+
+        OkHttpClient client = new  OkHttpClient();
+        String url = "https://world.openfoodfacts.org/api/v0/product/04963406/nutrition_grades";
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()) {
+                    Headers responseHeaders = response.headers();
+                    final ArrayList<String> arrHeaders = new ArrayList<>();
+                    for(int i = 0; i < responseHeaders.size(); i++) {
+                        arrHeaders.add(responseHeaders.value(i));
+                    }
+
+                    FoodActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvTest.setText(arrHeaders.get(0));
+                        }
+                    });
+                }
+            }
+        });
 
         recyclerView = findViewById(R.id.recycle_view_food);
         recyclerView.setHasFixedSize(true);
